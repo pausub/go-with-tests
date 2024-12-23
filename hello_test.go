484de36345d41892b66936a0c6e55a105ca41d52
Paulius Subačius <pausub@gmail.com>
2024-12-23 12:52:19 +0200

    add exclamatory mark to message

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Paulius")
-		want:= "Hello, Paulius"
+		want:= "Hello, Paulius!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
