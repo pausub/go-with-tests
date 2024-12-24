4e1089695e8170e5931750c5e62f8e05fc700d53
Paulius Subačius <pausub@gmail.com>
2024-12-24 12:04:33 +0200

    Iteration with benchmark and example

---
+++ iteration/iteration.go
@@ -0,0 +1,8 @@
+package iteration
+
+func Repeat(s string, times int) (result string) {
+	for i := 0; i < times; i++ {
+		result += s
+	}
+	return
+}
---
+++ iteration/iteration_test.go
@@ -0,0 +1,32 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIteration(t *testing.T) {
+	t.Run("repeat character 10 times", func(t *testing.T) {
+		got := Repeat("a", 10)
+		want := "aaaaaaaaaa"
+		assertCorrectResult(t, got, want)
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 100)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("a", 6)
+	fmt.Println(result)
+}
+
+func assertCorrectResult(t testing.TB, got, want string) {
+	if got != want {
+		t.Helper()
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
