a348ffdfc457ce3b389005286b6b30d8f30e0f1c
Paulius Subačius <pausub@gmail.com>
2024-12-26 13:04:05 +0200

    sums

--- sum/sum.go
+++ sum/sum.go
@@ -1,10 +1,32 @@
 package sum
 
-// array size is aprt of type. Its impossible to declare array 
+// array size is aprt of type. Its impossible to declare array
 // type without size.
 func Sum(a []int) (sum int) {
 	for _, num := range a {
 		sum += num
+	}
+	return
+}
+
+// Use append(slice, item) to add item to slice and not worry about length or capacity
+func SumAll(slices ...[]int) (sums []int) {
+	for _, nums := range slices {
+		sums = append(sums, Sum(nums))
+	}
+	return
+}
+
+// Slice a slice with [start:end] operator. 
+// Ommit numbers to capture everyting to that side
+func SumAllTails(slices ...[]int) (sums []int) {
+	for _, nums := range slices {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,19 +1,50 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("sum of numbers in array of size 5", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 		got := Sum(numbers)
 		want := 15
-		assertCorrectNumber(t, got, want, numbers)
+		assertCorrectNumbers(t, got, want, numbers)
 	})
 }
 
-func assertCorrectNumber(t testing.TB, got, want int, numbers []int) {
+func TestSumAll(t *testing.T) {
+	t.Run("Sum of all integers in slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{1, 0, 1})
+		want := []int{6, 2}
+		assertCorrectResult(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("Sum of all tails of integers in slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{1, 0, 1})
+		want := []int{5, 1}
+		assertCorrectResult(t, got, want)
+	})
+	t.Run("Sum of all tails of integers of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{9, 1})
+		want := []int{0, 1}
+		assertCorrectResult(t, got, want)
+	})
+}
+
+func assertCorrectNumbers(t testing.TB, got, want int, numbers []int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d, numbers: %v", got, want, numbers)
+	}
+}
+
+func assertCorrectResult(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
