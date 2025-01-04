32aedc0897f3648b662fde44e288191f39e8b7d2
Paulius Subačius <pausub@gmail.com>
2025-01-04 12:23:16 +0200

    Channels, select and httptest

--- select/main.go
+++ select/main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Race(url1, url2 string) (result string) {
-	duration1 := measureResponseTime(url1)
-	duration2 := measureResponseTime(url2)
+var tenSecondsDuration = 10 * time.Second
 
-	if duration1 > duration2 {
-		return url2
-	} else {
-		return url1
+func Race(url1, url2 string) (result string, err error) {
+	return ConfigurableRacer(url1, url2, tenSecondsDuration)
+}
+
+func ConfigurableRacer(url1, url2 string, timeout time.Duration) (result string, err error) {
+
+	// select allows to wait on multipe channels. First one
+	// to send value - wins and its code block gets executed
+	select {
+
+	// reader unblocks when receives data or when channel
+	// is closed
+	case <-ping(url1):
+		return url1, nil
+	case <-ping(url2):
+		return url2, nil
+
+	// wait at most 10 seconds, then return error.
+	case <-time.After(timeout):
+		return "", fmt.Errorf("time out after waiting for %d waiting for %q or %q", timeout, url1, url2)
 	}
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	// always create channel with make, othervice it will be
+	// nil and wont accept messages
+	// empty struct is cheaper chan boolean channel
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		// close channel after get returns
+		close(ch)
+	}()
+	// return channel
+	return ch
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -9,33 +9,50 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := makeDelayedServer(20)
-	fastServer := makeDelayedServer(0)
+	t.Run("compare get times and return faster", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	// nice way to instruct to close resources magically at
-	// the end of the function for readability purposes
-	defer slowServer.Close()
-	defer fastServer.Close()
+		// nice way to instruct to close resources magically at
+		// the end of the function for readability purposes
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowUrl := slowServer.URL
-	fastUrl := fastServer.URL
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
 
-	want := fastUrl
-	got := Race(slowUrl, fastUrl)
+		want := fastUrl
+		got, err := Race(slowUrl, fastUrl)
 
-	if want != got {
-		t.Errorf("got: %q, want: %q", got, want)
-	}
+		if err != nil {
+			t.Errorf("expected nil error, got %q", err)
+		}
 
-	slowServer.Close()
-	fastServer.Close()
+		if want != got {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
+
+	t.Run("return error if does not respond in specified time", func(t *testing.T) {
+		delayedServer := makeDelayedServer(20 * time.Millisecond)
+
+		defer delayedServer.Close()
+
+		url := delayedServer.URL
+
+		_, err := ConfigurableRacer(url, url, 10 * time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected err, got nil")
+		}
+	})
 }
 
 // httptest.NewServer magically finds ope port and creates server on that
 // much amazed
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(delay * time.Millisecond)
+		time.Sleep(delay)
 		w.Write([]byte{})
 	}))
 }
